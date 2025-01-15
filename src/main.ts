a007e63831d21b541eda7bd8ba77fa4cbceb9743
Joyrudra Biswas <joyrudra@itobuz.com>
2025-01-15 12:30:17 +0530

    chore: initial commit, project configured with swc

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+import config from './config/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(config.app.port);
+  console.log(
+    `⚡️ Application is running on: http://localhost:${config.app.port} ⚡️`,
+  );
+}
+bootstrap();
